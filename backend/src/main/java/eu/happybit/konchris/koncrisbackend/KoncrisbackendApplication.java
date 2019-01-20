0529b264322e72225acbd0c5fc63423c83c87671
Aristotelis Christou <telischristou@gmail.com>
2019-01-20 23:04:15 +0200

    Remove setup code

--- backend/src/main/java/eu/happybit/konchris/koncrisbackend/KoncrisbackendApplication.java
+++ backend/src/main/java/eu/happybit/konchris/koncrisbackend/KoncrisbackendApplication.java
@@ -1,21 +1,17 @@
 package eu.happybit.konchris.koncrisbackend;
 
-import eu.happybit.konchris.koncrisbackend.entity.Role;
-import eu.happybit.konchris.koncrisbackend.entity.RoleName;
-import eu.happybit.konchris.koncrisbackend.entity.User;
 import eu.happybit.konchris.koncrisbackend.repository.RoleRepository;
 import eu.happybit.konchris.koncrisbackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.annotation.PostConstruct;
-import java.util.*;
+import java.util.TimeZone;
 
 @SpringBootApplication
-public class KoncrisbackendApplication implements CommandLineRunner {
+public class KoncrisbackendApplication {
   @Autowired
   RoleRepository roleRepository;
 
@@ -34,19 +30,4 @@ public class KoncrisbackendApplication implements CommandLineRunner {
     SpringApplication.run(KoncrisbackendApplication.class, args);
   }
 
-  @Override
-  public void run (String... args) throws Exception {
-//    // Create users
-//    User simpleUser = new User ();
-//    simpleUser.setFirstName ("Jane");
-//    simpleUser.setLastName ("Doe");
-//    simpleUser.setEmail ("dev@example.com");
-//    simpleUser.setUsername ("user");
-//    simpleUser.setPassword (passwordEncoder.encode("123456"));
-//
-//    Set<Role> roleSet = new HashSet<> ();
-//    roleSet.add (roleRepository.findByName (RoleName.ROLE_USER).get());
-//    simpleUser.setRoles (roleSet);
-//    userRepository.save (simpleUser);
-  }
 }
