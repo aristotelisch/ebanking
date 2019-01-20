478c210ee0aeb17ac284c74afbbae2637d3f8411
Aristotelis Christou <telischristou@gmail.com>
2019-01-20 23:11:40 +0200

    Optimize imports, remove redudant port setting from dev properties

--- backend/src/main/java/eu/happybit/konchris/koncrisbackend/KoncrisbackendApplication.java
+++ backend/src/main/java/eu/happybit/konchris/koncrisbackend/KoncrisbackendApplication.java
@@ -1,25 +1,13 @@
 package eu.happybit.konchris.koncrisbackend;
 
-import eu.happybit.konchris.koncrisbackend.repository.RoleRepository;
-import eu.happybit.konchris.koncrisbackend.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
 
 @SpringBootApplication
 public class KoncrisbackendApplication {
-  @Autowired
-  RoleRepository roleRepository;
-
-  @Autowired
-  UserRepository userRepository;
-
-  @Autowired
-  PasswordEncoder passwordEncoder;
 
   @PostConstruct
   void init() {
@@ -29,5 +17,4 @@ public class KoncrisbackendApplication {
   public static void main(String[] args) {
     SpringApplication.run(KoncrisbackendApplication.class, args);
   }
-
 }
